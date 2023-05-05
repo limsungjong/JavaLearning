af7755f09cf9fd1c5706297419ad0a42462b7a04
Van <sung8881@naver.com>
2023-05-05 12:11:38 +0900

    create BubbleSort, SelectionSort

---
+++ src/Sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] data = new int[]{5, 3, 5, 2, 5, 7, 2, 1};
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length - 1; j++) {
+                if(data[i] < data[j]) {
+                    int temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(data));
+    }
+}
---
+++ src/Sort/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] data = new int[] {5,2,1,5,7,2,5,3};
+
+        for(int i = 0; i < data.length - 1; i++) {
+            int count = i;
+            for(int j = i + 1; j < data.length; j++) {
+                if(data[j] < data[count]) {
+                    count = j;
+                }
+            }
+            if(data[i] > data[count]) {
+                int temp = data[i];
+                data[i] = data[count];
+                data[count] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(data));
+    }
+}
