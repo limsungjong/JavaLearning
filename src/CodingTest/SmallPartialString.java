e957cd4894be9a65fc7ec3808ff402c6ba3a8d6b
Van <sung8881@naver.com>
2023-05-08 11:58:48 +0900

    프로그래머스 코딩테스트 solution 설명추가 level 1 : 크기가 작은 문자열 https://school.programmers.co.kr/learn/courses/30/lessons/147355

--- src/CodingTest/SmallPartialString.java
+++ src/CodingTest/SmallPartialString.java
@@ -13,14 +13,34 @@ package CodingTest;
     case 1 t = "10203" p = "15" => 3
  */
 public class SmallPartialString {
+    public static void main(String[] args) {
+        String t1 = "3141592";
+        String p1 = "271";
+        System.out.println(solution(t1, p1));
+
+        String t2 = "500220839878";
+        String p2 = "7";
+        System.out.println(solution(t2, p2));
+
+        String t3 = "10203";
+        String p3 = "15";
+        System.out.println(solution(t3, p3));
+    }
+
+    /*
+    일반적인 방법으로는 쉽게 풀수 있었다. 다만 나는 중간에 런타임 에러가 발생했는데
+    찾아보니 이유는 peekLong 이 부분과 Long.parseLong(sub) 이 부분을 long타입이 아닌
+    int 타입으로 인해 발생하는 문제였다. 값이 너무 크기 때문에 int로는 담을수 없는 범위의 값이
+    들어와서 계속 런타임에러가 발생했다. 이 2가지 부분을 long타입으로 변경해서 해결할 수 있었다.
+     */
     public static int solution(String t, String p) {
-        long peekInt = Long.parseLong(p);
+        long peekLong = Long.parseLong(p);
         long tLength = t.length();
         long pLength = p.length();
         int count = 0;
         for (int i = 0; i <= tLength - pLength; i++) {
-            String sub = t.substring(i, i + (int)pLength);
-            if(Long.parseLong(sub) <= peekInt) {
+            String sub = t.substring(i, i + (int) pLength);
+            if (Long.parseLong(sub) <= peekLong) {
                 count++;
             }
         }
