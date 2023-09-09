d6446401ba02c538d0cdab6e09e78e9ea789c958
limsungjong <sung8881@naver.com>
2023-09-09 17:28:59 +0900

    병합 정렬 예제 작성

--- src/Algorithms/Sort/MergeSort.java
+++ src/Algorithms/Sort/MergeSort.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {9, 5, 6, 7, 4, 1, 2, 3};
-        mergeSort1(arr);
+        mergeSort2(arr);
 
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void mergeSort1(int[] arr) {
+    public static void mergeSort2(int[] arr) {
         if (arr.length <= 1) {
             return;
         }
@@ -18,22 +18,17 @@ public class MergeSort {
         int middle = arr.length / 2;
         int[] left = Arrays.copyOfRange(arr, 0, middle);
         int[] right = Arrays.copyOfRange(arr, middle, arr.length);
-
-        mergeSort1(left);
-        mergeSort1(right);
+        mergeSort2(left);
+        mergeSort2(right);
 
         merge(arr, left, right);
-    }
-
-    public static void mergeSort2() {
-
     }
 
     public static void merge(int[] arr, int[] left, int[] right) {
         int i = 0, j = 0, k = 0;
 
         while (i < left.length && j < right.length) {
-            if (left[i] < (right[j])) {
+            if(left[i] < right[j]) {
                 arr[k] = left[i];
                 i++;
             } else {
@@ -45,14 +40,14 @@ public class MergeSort {
 
         while (i < left.length) {
             arr[k] = left[i];
-            i++;
             k++;
+            i++;
         }
 
         while (j < right.length) {
             arr[k] = right[j];
-            j++;
             k++;
+            j++;
         }
     }
 }
