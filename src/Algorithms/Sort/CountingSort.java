00b16a3102eb498345445d9a965521bbe4310133
limsungjong <sung8881@naver.com>
2023-09-19 23:25:32 +0900

    카운팅 정렬 예제 추가

---
+++ src/Algorithms/Sort/CountingSort.java
@@ -0,0 +1,11 @@
+package Algorithms.Sort;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 3, 4, 1, 2, 4, 1, 2, 3, 4, 5, 2, 1, 2, 3,};
+    }
+
+    public static void countingSort(int[] arr) {
+
+    }
+}
