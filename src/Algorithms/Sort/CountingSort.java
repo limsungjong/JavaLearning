f5ab95909ea37a04d8787d9c7523ae6abe10b73e
limsungjong <sung8881@naver.com>
2023-09-20 23:56:41 +0900

    카운팅 정렬 예제 변경

--- src/Algorithms/Sort/CountingSort.java
+++ src/Algorithms/Sort/CountingSort.java
@@ -1,11 +1,22 @@
 package Algorithms.Sort;
 
+import java.util.Arrays;
+
 public class CountingSort {
     public static void main(String[] args) {
         int[] arr = {1, 4, 2, 3, 4, 1, 2, 4, 1, 2, 3, 4, 5, 2, 1, 2, 3,};
+        int[] answer = countingSort(arr);
+
+        for(int i = 0; i < answer.length; i++) {
+            System.out.println(i + 1 + " : " + answer[i]);
+        }
     }
 
-    public static void countingSort(int[] arr) {
-
+    public static int[] countingSort(int[] arr) {
+        int[] counting = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            counting[arr[i] - 1]++;
+        }
+        return counting;
     }
 }
