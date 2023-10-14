dd961792299e17aff8d2d0754e735a1e5f545d1e
limsungjong <sung8881@naver.com>
2023-10-14 16:39:41 +0900

    코딩 테스트 문제 설명 변경

--- src/CodingTest/Level1/AdditionOfDigits.java
+++ src/CodingTest/Level1/AdditionOfDigits.java
@@ -2,8 +2,8 @@ package CodingTest.Level1;
 
 /*
     프로그래머스 level : 1
-    https://school.programmers.co.kr/learn/courses/30/lessons/86051
-    없는 숫자 더하기
+    https://school.programmers.co.kr/learn/courses/30/lessons/12931
+    자릿수 더하기
 
     자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
     예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
@@ -12,13 +12,12 @@ package CodingTest.Level1;
     N의 범위 : 100,000,000 이하의 자연수
 
     case 1
-    numbers = [1,2,3,4,6,7,8,0]
-    result = 14
-    5와 9가 없으므로 5 + 9 = 14를 return
+    n = 123
+    result = 6
 
-    case 1
-    numbers = [5,8,4,0,6,7,9]
-    1,2,3이 없으므로 1 + 2 + 3 = 6을 return
+    case 2
+    numbers = 987
+    result = 24
 */
 
 public class AdditionOfDigits {
